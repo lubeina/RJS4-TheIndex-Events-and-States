cbe9b849f4140d0771c9192360a235af777216bb
lubeina <52536093+lubeina@users.noreply.github.com>
2020-02-12 16:53:38 +0300

    Task Done

--- src/AuthorList.js
+++ src/AuthorList.js
@@ -1,31 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 
 // Components
 import AuthorCard from "./AuthorCard";
 import SearchBar from "./SearchBar";
 
-const AuthorList = props => {
-  const authorCards = props.authors.map(author => (
-    <AuthorCard
-      key={author.first_name + author.last_name}
-      author={author}
-      selectAuthor={props.selectAuthor}
-    />
-  ));
-
-  const filterAuthors = query => {
-    const filteredAuthors = props.authors.filter(author =>
-      `${author.first_name} ${author.last_name}`.includes(query)
-    );
-    console.log(filteredAuthors);
+class AuthorList extends Component {
+  state = {
+    filteredAuthors: this.props.authors
   };
-  return (
-    <div className="authors">
-      <h3>Authors</h3>
-      <SearchBar filterAuthors={filterAuthors} />
-      <div className="row">{authorCards}</div>
-    </div>
-  );
-};
+
+  filterAuthors = query => {
+    const filteredAuthors = this.props.authors.filter(author =>
+      `${author.first_name} ${author.last_name}`
+        .toLowerCase()
+        .includes(query.toLowerCase())
+    );
+    this.setState({ filteredAuthors: filteredAuthors });
+  };
+  render() {
+    const authorCards = this.state.filteredAuthors.map(author => (
+      <AuthorCard
+        key={author.first_name + author.last_name}
+        author={author}
+        selectAuthor={this.props.selectAuthor}
+      />
+    ));
+    return (
+      <div className="authors">
+        <h3>Authors</h3>
+        <SearchBar filterAuthors={this.filterAuthors} />
+        <div className="row">{authorCards}</div>
+      </div>
+    );
+  }
+}
 
 export default AuthorList;
